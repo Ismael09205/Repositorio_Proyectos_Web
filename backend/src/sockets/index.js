b9dd310da517de7410e9f76bd926e39f62ff6d08
Joel2775 <joel.acosta@epn.edu.ec>
2026-07-17 23:18:16 -0500

    Delete backend directory

--- backend/src/sockets/index.js
+++
@@ -1,8 +0,0 @@
-const registrarChatSocket = require('./chat.socket');
-
-// Registra todos los manejadores de eventos WebSocket de la aplicación
-const registrarSockets = (io) => {
-    registrarChatSocket(io);
-};
-
-module.exports = registrarSockets;
