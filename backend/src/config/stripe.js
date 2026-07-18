42e18368dd6d0fefc89ccd01aef9cf0213748604
Joel2775 <joel.acosta@epn.edu.ec>
2026-07-17 23:31:00 -0500

    Delete backend directory

--- backend/src/config/stripe.js
+++
@@ -1,7 +0,0 @@
-const Stripe = require('stripe');
-require('dotenv').config();
-
-// Inicializamos Stripe con la llave secreta de pruebas
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-module.exports = stripe;
--- backend/src/routes/payment.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const paymentController = require('../controllers/payment.controller');
-
-// Ruta para iniciar el proceso de donación
-router.post('/create-checkout-session', paymentController.createDonationSession);
-
-module.exports = router;
